ab2e1de49257376feeb44f2f8dce82293e79c41c
William Saran <williamsaranjr@gmail.com>
2024-09-06 14:47:10 -0300

    feat: finalizando a implementação do Feign Client

--- src/main/java/com/williamsaran/bancojaver/feign/ClienteFeign.java
+++ src/main/java/com/williamsaran/bancojaver/feign/ClienteFeign.java
@@ -3,8 +3,7 @@ package com.williamsaran.bancojaver.feign;
 import com.williamsaran.bancojaver.model.ClienteDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "cliente", url = "http://localhost:8000/clientes/")
 public interface ClienteFeign {
@@ -12,4 +11,19 @@ public interface ClienteFeign {
             value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     ClienteDTO encontrarClientePorId(@PathVariable Long id);
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    ClienteDTO criarConta(@RequestBody ClienteDTO dto);
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    ClienteDTO atualizarConta(@RequestBody ClienteDTO dto);
+
+    @DeleteMapping(value = "{id}")
+    void deletarPorId(@PathVariable Long id);
+
+    @PatchMapping(value = "desativar/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    ClienteDTO desativarConta(@PathVariable Long id);
 }
