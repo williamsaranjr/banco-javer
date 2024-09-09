e9b88b8d8e67ddd83388e2d807b30b01892482b5
William Saran <williamsaranjr@gmail.com>
2024-09-09 11:21:26 -0300

    docs: adicionando a documentação dos endpoints

---
+++ src/main/java/com/williamsaran/bancojaver/config/OpenAPIConfig.java
@@ -0,0 +1,17 @@
+package com.williamsaran.bancojaver.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Banco Javer")
+                        .description("API para gerenciamento de contas bancárias"));
+    }
+}
